6bd60d61b7cd97a70265b812c7fc6ee25ba625e5
AlexandraFelecan <ale.neaga25@gmail.com>
2025-07-30 20:19:38 +0300

    Create ApplicationManager.java

---
+++ src/test/java/org/example/managers/ApplicationManager.java
@@ -0,0 +1,144 @@
+package org.example.managers;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.asserts.SoftAssert;
+
+
+
+import java.util.List;
+import java.util.Random;
+
+public class ApplicationManager {
+    private static WebDriver driver;
+
+    // SELECTORS
+    public static String Burger_MENU = "#react-burger-menu-btn";
+
+    // ATTRIBUTES
+    public static String PLACEHOLDER = "placeholder";
+    public static String CURSOR = "cursor";
+    public static String VALUE = "value";
+
+
+    // CSS
+    public static String BACKGROUND_COLOR = "background-color";
+    public static String COLOR = "color";
+    public static String BORDER_COLOR = "border-color";
+    public static String POINTER = "pointer";
+
+
+    //SOFT ASSERT
+    public static SoftAssert softAssert = new SoftAssert();
+
+    public ApplicationManager(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void writeOnElement(String selector, String text) {
+        WebElement element = driver.findElement(By.cssSelector(selector));
+        element.clear();
+        element.sendKeys(text);
+    }
+
+    public void writeOnElementList(String selector, String text, int index) {
+        List<WebElement> element = driver.findElements(By.cssSelector(selector));
+        element.get(index).clear();
+        element.get(index).sendKeys(text);
+    }
+
+    public void checkTextOnElement(String selector, String text) {
+        WebElement element = driver.findElement(By.cssSelector(selector));
+        Assert.assertEquals(element.getText(), text);
+    }
+
+    public String getTextOnElement(String selector, int index) {
+        List<WebElement> elements = driver.findElements(By.cssSelector(selector));
+        return (elements.get(index).getText());
+    }
+
+    public String getTextOnElement(String selector) {
+        WebElement element = driver.findElement(By.cssSelector(selector));
+        return (element.getText());
+    }
+
+    public void checkTextOnElementsList(String selector, String text, int index) {
+        List<WebElement> elements = driver.findElements(By.cssSelector(selector));
+        Assert.assertEquals(elements.get(index).getText(), text);
+    }
+
+    public void clickOnElement(String selector) {
+        WebElement element = driver.findElement(By.cssSelector(selector));
+        element.click();
+    }
+
+    public void clickOnElementList(String selector, int index) {
+        List<WebElement> element = driver.findElements(By.cssSelector(selector));
+        element.get(index).click();
+    }
+
+    public void checkAttributeOnElement(String selector, String attribute, String attributeValue) {
+        WebElement element = driver.findElement(By.cssSelector(selector));
+        softAssert.assertEquals(element.getAttribute(attribute), attributeValue);
+    }
+
+    public void checkAttributeOnElementList(String selector, String attribute, String attributeValue, int index) {
+        List<WebElement> elements = driver.findElements(By.cssSelector(selector));
+        Assert.assertEquals(elements.get(index).getAttribute(attribute), attributeValue);
+    }
+
+    public String getAttributeOnElementList(String selector, String attribute, int index) {
+        List<WebElement> elements = driver.findElements(By.cssSelector(selector));
+        return (elements.get(index).getAttribute(attribute));
+    }
+
+    public String getAttributeOnElement(String selector, String attribute) {
+        WebElement element = driver.findElement(By.cssSelector(selector));
+        return (element.getAttribute(attribute));
+    }
+    public String getCSSValue(String selector, String attribute) {
+        WebElement element = driver.findElement(By.cssSelector(selector));
+        return element.getCssValue(attribute);
+    }
+    public String getCSSValue(String selector, String attribute, int index) {
+        List<WebElement> elements = driver.findElements(By.cssSelector(selector));
+        return (elements.get(index).getCssValue(attribute));
+    }
+
+    public void checkCSSValueOnElement(String selector, String attribute, String attributeValue) {
+        WebElement element = driver.findElement(By.cssSelector(selector));
+        softAssert.assertEquals(element.getCssValue(attribute), attributeValue);
+    }
+
+    public void checkCSSValueOnElementList(String selector, String attribute, String attributeValue, int index) {
+        List<WebElement> element = driver.findElements(By.cssSelector(selector));
+        softAssert.assertEquals(element.get(index).getCssValue(attribute), attributeValue);
+    }
+
+
+    public int getIntFromString(String text) {
+        return Integer.parseInt(text.replaceAll("[^0-9]", ""));
+    }
+
+    public double getDoubleFromString(String text) {
+        return Double.parseDouble(text.replaceAll("[^0-9]", ""));
+    }
+
+    public int randomInt(int min, int max) {
+        Random rand = new Random();
+        return (rand.nextInt(max - min + 1) + min);
+    }
+
+    public void selectElementsOnDropdown(String selector, String value) {
+        WebElement element = driver.findElement(By.cssSelector(selector));
+        Select select = new Select(element);
+        select.selectByVisibleText(value);
+    }
+
+    public int getElementsSize(String selector) {
+        return driver.findElements(By.cssSelector(selector)).size();
+    }
+}
