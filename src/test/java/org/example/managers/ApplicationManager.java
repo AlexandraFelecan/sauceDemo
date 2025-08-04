b2ea26ca3a979b8ba94161c8bbea96bd5a938f9f
AlexandraFelecan <ale.neaga25@gmail.com>
2025-08-04 19:54:44 +0300

    Update ApplicationManager.java

--- src/test/java/org/example/managers/ApplicationManager.java
+++ src/test/java/org/example/managers/ApplicationManager.java
@@ -13,7 +13,8 @@ import java.util.List;
 import java.util.Random;
 
 public class ApplicationManager {
-    private static WebDriver driver;
+    private WebDriver driver;
+
 
     // SELECTORS
     public static String Burger_MENU = "#react-burger-menu-btn";
