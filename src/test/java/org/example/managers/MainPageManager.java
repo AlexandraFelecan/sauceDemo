b9edf3feb4da36e0dc09c4c55edfc217f36f3021
AlexandraFelecan <ale.neaga25@gmail.com>
2025-08-06 19:54:53 +0300

    Create MainPageManager.java

---
+++ src/test/java/org/example/managers/MainPageManager.java
@@ -0,0 +1,12 @@
+package org.example.managers;
+
+public class MainPageManager {
+
+    // SELECTORS
+
+    public static String sort_BTN = ".product_sort_container";
+    public static String items_price_list = ".inventory_item_price";
+    public static String items_name_list = ".inventory_item_name";
+    public static String add_to_cart_buttons = ".btn_primary";
+    public static String remove_btn = ".btn_secondary";
+}
