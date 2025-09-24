297935450a5bc629ebcffda6713b3a999c7e9a75
AlexandraFelecan <ale.neaga25@gmail.com>
2025-09-24 18:23:05 +0300

    Update WaitUtils.java

--- src/test/java/org/example/utils/WaitUtils.java
+++ src/test/java/org/example/utils/WaitUtils.java
@@ -2,13 +2,11 @@ package org.example.utils;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
-import java.util.List;
 
 public class WaitUtils {
 
