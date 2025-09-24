b1164700a6dd2ff650c15456b4016dbeee2013d3
AlexandraFelecan <ale.neaga25@gmail.com>
2025-09-24 18:21:35 +0300

    Update WaitUtils.java

--- src/test/java/org/example/utils/WaitUtils.java
+++ src/test/java/org/example/utils/WaitUtils.java
@@ -23,18 +23,8 @@ public class WaitUtils {
         new WebDriverWait(driver, Duration.of(WAIT_TIMEOUT, ChronoUnit.SECONDS)).until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector(selector)));
     }
 
-    public static void checkInvisibility(String selector, int waitTimeout, WebDriver driver) {
-        new WebDriverWait(driver, Duration.ofSeconds(waitTimeout))
-                .until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector(selector)));
-    }
-
     public static void elementLocatedWithText(String selector, String text, WebDriver driver) {
         new WebDriverWait(driver, Duration.of(WAIT_TIMEOUT, ChronoUnit.SECONDS)).until(ExpectedConditions.textToBePresentInElementLocated(By.cssSelector(selector), text));
     }
 
-    public static void elementsLocatedWithText(String selector, String text, int index, WebDriver driver) {
-        List<WebElement> elements = driver.findElements(By.cssSelector(selector));
-        new WebDriverWait(driver, Duration.of(WAIT_TIMEOUT, ChronoUnit.SECONDS)).until(ExpectedConditions.textToBePresentInElement(elements.get(index), text));
-
-    }
 }
